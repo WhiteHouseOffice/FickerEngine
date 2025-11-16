f38563be745ddefc3edbb0979e63b8861ceb7848
WhiteHouseOffice <100376524+WhiteHouseOffice@users.noreply.github.com>
2025-11-16 12:01:34 +0000

    game: remove stray Game methods from GameObject.cpp

--- engine/game/GameObject.cpp
+++ engine/game/GameObject.cpp
@@ -1,39 +1,5 @@
-#include "game/Game.h"
-#include "core/Input.h"
-#include "math/MiniMath.h"
+#include "game/GameObject.h"
 
-#include <cstdio>
-
-void Game::init() {
-  camPos    = Vec3{0.f, 2.f, 5.f};
-  yaw       = 0.0f;
-  logTimer  = 0.0f;
-}
-
-void Game::update(float dt) {
-  Vec3 delta{0.f, 0.f, 0.f};
-  const float speed = 3.0f;
-
-  // Simple WASD movement in XZ plane
-  if (Input::isKeyDown(Input::KEY_W)) delta.z -= speed * dt;
-  if (Input::isKeyDown(Input::KEY_S)) delta.z += speed * dt;
-  if (Input::isKeyDown(Input::KEY_A)) delta.x -= speed * dt;
-  if (Input::isKeyDown(Input::KEY_D)) delta.x += speed * dt;
-
-  camPos += delta;
-
-  // Occasionally log camera position
-  logTimer += dt;
-  if (logTimer >= 1.0f) {
-    logTimer = 0.0f;
-    std::printf("[camera] pos = (%.2f, %.2f, %.2f)\n",
-                camPos.x, camPos.y, camPos.z);
-  }
-}
-
-Mat4 Game::view() const {
-  const Vec3 forward{0.f, 0.f, -1.f};
-  const Vec3 up{0.f, 1.f, 0.f};
-  const Vec3 target = camPos + forward;
-  return lookAt(camPos, target, up);
+void GameObject::update(float /*dt*/) {
+  // TODO: per-object behaviour will go here later.
 }
