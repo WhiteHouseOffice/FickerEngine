ddf71900ea4333a3f9f5bbb86716abe1c63005d2
WhiteHouseOffice <100376524+WhiteHouseOffice@users.noreply.github.com>
2025-12-21 19:20:55 +0000

    Fix Scene virtual method definitions (no inline bodies in header)

--- engine/game/Scene.cpp
+++ engine/game/Scene.cpp
@@ -1,23 +1,6 @@
 #include "game/Scene.h"
 
-void Scene::init() {
-  // If you already have a different method that sets up objects,
-  // call it here (or keep empty for now).
-}
-
-// If you already have camera or input-driven state, update it here.
-void Scene::update(float /*dt*/) {
-  // TODO: camera movement / physics later
-}
-
-void Scene::render(const Mat4& view, const Mat4& proj) {
-    (void)view;
-    (void)proj;
-
-    // draw stuff (same code as before)
-}
-
-
-void Scene::renderDebug(const Mat4& /*view*/, const Mat4& /*proj*/) {
-  // TODO: draw grid / marker debug here
-}
+void Scene::init() {}
+void Scene::update(float /*dt*/) {}
+void Scene::render(const Mat4& /*view*/, const Mat4& /*proj*/) {}
+void Scene::renderDebug(const Mat4& /*view*/, const Mat4& /*proj*/) {}
--- engine/game/Scene.h
+++ engine/game/Scene.h
@@ -4,13 +4,10 @@
 
 class Scene {
 public:
-    virtual ~Scene() = default;
+  virtual ~Scene() = default;
 
-    // Lifecycle
-    virtual void init() {}
-    virtual void update(float /*dt*/) {}
-
-    // Rendering
-    virtual void render(const Mat4& /*view*/, const Mat4& /*proj*/) {}
-    virtual void renderDebug(const Mat4& /*view*/, const Mat4& /*proj*/) {}
+  virtual void init();
+  virtual void update(float dt);
+  virtual void render(const Mat4& view, const Mat4& proj);
+  virtual void renderDebug(const Mat4& view, const Mat4& proj);
 };
